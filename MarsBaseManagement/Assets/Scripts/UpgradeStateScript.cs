9c89b1f48d0688ad2a5e1662a314962312fd1132
m6gorowski <m6gorowski@protonmail.com>
2024-12-07 00:48:10 +0100

    Switch some buttons and other images

---
+++ MarsBaseManagement/Assets/Scripts/UpgradeStateScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeStateScript : MonoBehaviour
+{
+    [SerializeField]
+    private string[] _descriptions;
+    [SerializeField]
+    private int[] _levels;
+    [SerializeField]
+    private Button _buyButton;
+    [SerializeField]
+    private string[] _buyButtonDesc;
+    [SerializeField]
+    private 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
