11985452649ca02549207fa67cb2ea233111bf4e
m6gorowski <m6gorowski@protonmail.com>
2024-12-07 05:29:30 +0100

    somewhat working stars parallax

--- MarsBaseManagement/Assets/Scripts/ParallaxScript.cs
+++ MarsBaseManagement/Assets/Scripts/ParallaxScript.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ParallaxScript : MonoBehaviour
 {
     [SerializeField] private float _scrollSpeed = -2f;
     private float _singleTextureWidth;
+    private RectTransform _rectTransform;
     void Start()
     {
+        _rectTransform = GetComponent<RectTransform>();
         SetupTexture();
     }
 
@@ -18,20 +21,27 @@ public class ParallaxScript : MonoBehaviour
     }
     void SetupTexture()
     {
-        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
-        _singleTextureWidth = sprite.texture.width / (sprite.pixelsPerUnit * 3);
+        Image image = GetComponent<Image>();
+        if (image != null && image.sprite != null)
+        {
+            _singleTextureWidth = image.sprite.rect.width / image.pixelsPerUnit / 3f;
+        }
+        else
+        {
+            Debug.LogWarning("No Image or Sprite assigned to the UI element.");
+        }
     }
     void Scroll()
     {
-        float delta = (_scrollSpeed * Time.deltaTime);
-        transform.position += new Vector3(delta, 0f, 0f);
+        float delta = _scrollSpeed * Time.deltaTime;
+        _rectTransform.localPosition += new Vector3(delta, 0f, 0f);
     }
 
     void CheckReset()
     {
-        if ((Mathf.Abs(transform.position.x) - _singleTextureWidth) > 0)
+        if (Mathf.Abs(_rectTransform.localPosition.x) >= _singleTextureWidth)
         {
-            transform.position = new Vector3(0f, transform.position.y, transform.position.z);
+            _rectTransform.localPosition = new Vector3(0f, _rectTransform.localPosition.y, _rectTransform.localPosition.z);
         }
     }
 }
